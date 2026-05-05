d830a8a6f84e74192ddc0dbdeccca7d9448e14ad
hussein-ashraf1200 <hussein061200@gmail.com>
2026-05-05 11:18:49 +0300

    fix middleware matcher

--- middleware.js
+++ middleware.js
@@ -4,10 +4,13 @@ const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect(); // ← شيل الـ () من auth وخلي protect() بس
+    await auth.protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
