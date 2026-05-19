2e50f668f6757fbe9814c125693d4af1366c07b8
hussein-ashraf1200 <hussein061200@gmail.com>
2026-05-19 15:14:16 +0300

    fix middleware bot blocking

--- middleware.js
+++ middleware.js
@@ -3,6 +3,18 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
+  const userAgent = req.headers.get("user-agent") || "";
+
+  // 👇 allow social media bots
+  const isBot =
+    userAgent.includes("facebookexternalhit") ||
+    userAgent.includes("Facebot") ||
+    userAgent.includes("WhatsApp");
+
+  // لو bot → سيبه يعدي
+  if (isBot) return;
+
+  // حماية الداشبورد فقط
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
