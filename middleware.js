648de8f6fe9b8a7e17fc8e0ec674e400c995e861
hussein-ashraf1200 <hussein061200@gmail.com>
2026-05-19 15:28:18 +0300

    robots fixed and middleware

--- middleware.js
+++ middleware.js
@@ -1,28 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const userAgent = req.headers.get("user-agent") || "";
 
-  // 👇 allow social media bots
   const isBot =
     userAgent.includes("facebookexternalhit") ||
     userAgent.includes("Facebot") ||
-    userAgent.includes("WhatsApp");
+    userAgent.includes("WhatsApp") ||
+    userAgent.includes("Twitterbot") ||
+    userAgent.includes("LinkedInBot");
 
-  // لو bot → سيبه يعدي
-  if (isBot) return;
+  // ✅ لازم NextResponse.next()
+  if (isBot) {
+    return NextResponse.next();
+  }
 
-  // حماية الداشبورد فقط
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
-
-export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
-};
