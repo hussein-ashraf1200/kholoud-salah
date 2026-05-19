7c04cbdf8a71a99773a6ae5dc176073b80ec7f39
hussein-ashraf1200 <hussein061200@gmail.com>
2026-05-19 12:50:53 +0300

    whatsapp image fixed

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,7 @@ export const metadata = {
     siteName: "Kholoud Salah Real Estate",
     images: [
       {
-        url: `${siteUrl}/og-img.png`, // ✅ صورة منفصلة
+        url: `${siteUrl}/og-images`, // ✅ صورة منفصلة
         width: 1200,
         height: 630,
         alt: "Kholoud Salah Real Estate - Ras El Hikma luxury properties",
